87dd2a040ee29ad05fec47a7ee318b6d9d5f3d71
auriellove <auriel.e.love@gmail.com>
2022-04-27 12:19:15 -0400

    added const and console log

---
+++ script.js
@@ -0,0 +1,15 @@
+const Name1="Auriel";
+const Name2="Johnathan";
+const Name3="Edward";
+
+console.log(Name1);
+console.log(Name2);
+console.log(Name3);
+
+if (Name2 > Name3); {
+    console.log('Johnathan has the longest name');
+} if (Name1 < Name2); {
+    console.log('Johnathan has the longest name');
+} if (Name1 === Name3); {
+    console.log('Auriel and Edward are the same length');
+}
